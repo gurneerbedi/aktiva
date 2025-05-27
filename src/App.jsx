8fcd721550e99338114e615218678df1f60cb19c
Gurneer <gurneer1223@gmail.com>
2025-05-27 15:39:24 -0400

    changed router paths, removed base /aktiva

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,16 @@ import Home from './pages/HomePage/HomePage.jsx';
 import Footer from './components/Footer/Footer.jsx';
 import Privacy from "./pages/PrivacyPage/PrivacyPage.jsx";
 import Terms from "./pages/TermsPage/TermsPage.jsx";
-// import Program from './pages/Program';
 import Amenities from './pages/AmenitiesPage/AmenitiesPage.jsx';
-// import Contact from './pages/Contact';
-
 
 function App() {
   return (
-    <Router basename="/aktiva">
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/privacy" element={<Privacy />} />
         <Route path="/terms" element={<Terms />} />
-         <Route path="/amenities" element={<Amenities />} />
-
-        {/* <Route path="/program" element={<Program />} />
-       
-        <Route path="/contact" element={<Contact />} /> */}
+        <Route path="/amenities" element={<Amenities />} />
       </Routes>
       <Footer/>
     </Router>
