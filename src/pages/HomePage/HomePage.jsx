db4b4a48e0c22ba22699d94d3875bee31e7780d2
Gurneer <gurneer1223@gmail.com>
2025-05-27 13:58:17 -0400

    styling on homepage w/ transform body

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,13 +2,17 @@ import "./HomePage.scss";
 import Banner from "../../components/Banner/Banner.jsx"
 import Buildyourbody from "../../components/Buildyourbody/Buildyourbody.jsx";
 import Amenities from "../../components/Amenities/Amenities.jsx";
+import HomeBackgroundImg from "../../assets/images/homebackground.jpeg"
 function HomePage() {
     return (
         <>
         <div className="homepage">
+            <div className="homepage__header">
+            
             <h1 className="homepage__heading-small">LIVE YOUR BEST LIFE</h1>
             <h1 className="homepage__heading-big">STAY ACTIVE</h1>
-            <button className="homepage__button">Start Your Training</button>
+            <button className="homepage__button">Start Your Training Today</button>
+            </div>
         </div>
         <Buildyourbody/>
         <Amenities></Amenities>
