471c052e0ec3a2055f83f404d20aea0e0ecc731b
Gurneer <gurneer1223@gmail.com>
2025-06-11 18:19:22 -0400

    compressed more images

--- src/pages/AmenitiesPage/AmenitiesPage.jsx
+++ src/pages/AmenitiesPage/AmenitiesPage.jsx
@@ -8,11 +8,11 @@ import sparoom from "../../assets/images/sparoom.jpg";
 import equipment from "../../assets/images/equipment.jpg";
 import snackbar from "../../assets/images/snackbar.jpg";
 import ondemand from "../../assets/images/ondemand.jpg";
-import spa2 from "../../assets/images/spa2.jpg";
-import snack2 from "../../assets/images/snack2.jpg";
-import snack3 from "../../assets/images/snack3.jpg";
-import snack4 from "../../assets/images/snack4.jpg";
-import snack5 from "../../assets/images/snack5.jpg";
+import spa2 from "../../assets/images/spa2min.jpg";
+import snack2 from "../../assets/images/snack2min.jpg";
+import snack3 from "../../assets/images/snack3min.jpg";
+import snack4 from "../../assets/images/snack4min.jpg";
+import snack5 from "../../assets/images/snack5min.jpg";
 
 import equip2 from "../../assets/images/equip2min.jpg";
 import equip3 from "../../assets/images/equip3min.jpg";
@@ -63,7 +63,7 @@ function Amenities() {
         "Goal-supportive nutrition",
         "Luxurious dining area",
       ],
-      images: [snackbar,  snack3, snack4, snack5],
+      images: [snackbar, snack3, snack4, snack5],
     },
   ];
 
